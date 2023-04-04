de5e0b0529e8529c33e72be2e7f0b66988564498
SAYL <sakovetclesha@gmail.com>
2023-04-04 21:07:31 +0300

    Ended program Shifrator

---
+++ Shifrator.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int ArrayLong;
+    int shift;
+
+    cout << "Введите длину слова\n";
+    cin >> ArrayLong;
+
+    cout << "Введите сдвиг букв\n";
+    cin >> shift;
+
+    char Array[ArrayLong - 1];
+
+    cout << "Введите слово\n";
+    cin >> Array;
+
+    for (int i = 0; i < ArrayLong; i++)
+    {
+        while (((Array[i] + shift) > 122) || (((Array[i] + shift) > 90) && ((Array[i] + shift) < 97)))
+            shift -= 26;
+
+        cout << char(Array[i] + shift);
+    }
+}
